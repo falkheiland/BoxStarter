b9fbb43c67beb05e13fb8e82bbcb2e12fdb74db7
falkheiland <falkheiland@users.noreply.github.com>
2020-10-09 12:33:52 +0200

    Update HyperV.ps1

--- scripts/HyperV.ps1
+++ scripts/HyperV.ps1
@@ -1,8 +1,1 @@
 choco install -y Microsoft-Hyper-V-All -source windowsfeatures
-choco install -y Microsoft-Hyper-V -source windowsfeatures
-choco install -y Microsoft-Hyper-V-Tools-All -source windowsfeatures
-choco install -y Microsoft-Hyper-V-Management-PowerShell -source windowsfeatures
-choco install -y Microsoft-Hyper-V-Hypervisor -source windowsfeatures
-choco install -y Microsoft-Hyper-V-Services -source windowsfeatures
-choco install -y Microsoft-Hyper-V-Management-Clients -source windowsfeatures
-choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
