6e7d3e9b6d0de3718b82bb9c1c64282734018f9a
falkheiland <falkheiland@users.noreply.github.com>
2020-10-09 12:58:18 +0200

    Update WSL.ps1

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,16 +1,18 @@
-choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-#--- Ubuntu ---
-# TODO: Move this to choco install once --root is included in that package
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
-# run the distro once and have it install locally with root user, unset password
+#choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+choco install wsl2 --params "/Version:2 /Retry:true"
+#dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+#dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 #RefreshEnv
 #wsl --set-default-version 2
 #https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
+
+#--- Ubuntu ---
+# TODO: Move this to choco install once --root is included in that package
+choco install wsl-ubuntu-1804
+#Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
+#Add-AppxPackage -Path ~/Ubuntu.appx
+# run the distro once and have it install locally with root user, unset password
 
 RefreshEnv
 Ubuntu1804 install --root
