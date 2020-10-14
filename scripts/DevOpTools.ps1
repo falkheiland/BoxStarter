07bc1675f36833159a12f86b02bb63b6fe8b9c56
falkheiland <falkheiland@users.noreply.github.com>
2020-10-14 17:20:27 +0200

    Update DevOpTools.ps1

--- scripts/DevOpTools.ps1
+++ scripts/DevOpTools.ps1
@@ -37,8 +37,8 @@ choco install -y winscp
 $DocumentPath = ('{0}\documents\PowerShell' -f $HOME)
 New-Item -ItemType Directory -Path $DocumentPath -ErrorAction SilentlyContinue
 $ProfileFile = '{0}\documents\profile.ps1' -f $HOME
-#Invoke-WebRequest -Uri https://gist.github.com/falkheiland/d221d84a7b2a383854747c127a612a89 -OutFile $ProfileFile -UseBasicParsing
-Invoke-WebRequest -Uri https://gist.githubusercontent.com/falkheiland/d221d84a7b2a383854747c127a612a89/raw/409d24311da6171c4ca5156e61d226d4aa2a449e/WindowsPowerShell%2520Microsoft.PowerShell_profile.ps1 -OutFile $ProfileFile -UseBasicParsing
+$PsUri = 'https://gist.githubusercontent.com/falkheiland/d221d84a7b2a383854747c127a612a89/raw/409d24311da6171c4ca5156e61d226d4aa2a449e/WindowsPowerShell%2520Microsoft.PowerShell_profile.ps1'
+Invoke-WebRequest -Uri $PsUri -OutFile $ProfileFile -UseBasicParsing
 @(
   ('{0}\documents\Powershell\Microsoft.PowerShell_profile.ps1' -f $HOME),
   ('{0}\documents\Powershell\Microsoft.PowerShell_profile.ps1' -f $HOME),
@@ -53,8 +53,8 @@ Remove-Item -Path $ProfileFile
 # Copy Windows Terminal settings.json
 
 $SettingsFile = '{0}\documents\settings.json' -f $HOME
-$SettingsDestination = '{0}\documents\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\' -f $HOME
-#Invoke-WebRequest -Uri https://gist.github.com/falkheiland/a227aeb50dfb380b66a6f4e9c983acff -OutFile $SettingsFile -UseBasicParsing
-Invoke-WebRequest -Uri https://gist.githubusercontent.com/falkheiland/a227aeb50dfb380b66a6f4e9c983acff/raw/54bdc68468ee8aba8687b09ca8e39a294c22b46b/Windows%2520Terminal%2520settings.json -OutFile $SettingsFile -UseBasicParsing
+$SettingsDestination = '{0}\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\' -f $HOME
+$WtUri = 'https://gist.githubusercontent.com/falkheiland/a227aeb50dfb380b66a6f4e9c983acff/raw/54bdc68468ee8aba8687b09ca8e39a294c22b46b/Windows%2520Terminal%2520settings.json'
+Invoke-WebRequest -Uri $WtUri -OutFile $SettingsFile -UseBasicParsing
 Copy-Item -Path $SettingsFile -Destination ('{0}{1}' -f $SettingsDestination, 'settings.json')
 Remove-Item -Path $SettingsFile
