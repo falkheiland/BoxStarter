c9005168fe22888aee7b14fcae27334c9f2fa8e5
falkheiland <falkheiland@users.noreply.github.com>
2020-10-14 15:25:36 +0200

    added wt settings, PS profiles from gists

--- scripts/DevOpTools.ps1
+++ scripts/DevOpTools.ps1
@@ -35,3 +35,23 @@ choco install -y winscp
 Write-Host "## DevOpsTools | vscode extensions" -ForegroundColor "Yellow"
 
 code --install-extension shan.code-settings-sync
+
+# Copy (Windows)PowerShell Profil
+
+$ProfileFile = '{0}\profile.ps1' -f $HOME
+Invoke-WebRequest -Uri https://gist.github.com/falkheiland/d221d84a7b2a383854747c127a612a89 -OutFile $ProfileFile -UseBasicParsing
+@(
+  ('{0}\Documents\Powershell\Microsoft.PowerShell_profile.ps1' -f $HOME),
+  ('{0}\Documents\Powershell\Microsoft.VSCode_profile.ps1' -f $HOME),
+  ('{0}\Documents\WindowsPowershell\Microsoft.PowerShell_profile.ps1' -f $HOME)
+  ('{0}\Documents\WindowsPowershell\Microsoft.VSCode_profile.ps1' -f $HOME)
+) | ForEach-Object {
+  Copy-Item -Path $ProfileFile -Destination $_
+}
+
+# Copy Windows Terminal settings.json
+
+$SettingsFile = '{0}\settings.json' -f $HOME
+$SettingsDestination = '{0}\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\' -f $HOME
+Invoke-WebRequest -Uri https://gist.github.com/falkheiland/a227aeb50dfb380b66a6f4e9c983acff -OutFile $SettingsFile -UseBasicParsing
+Copy-Item -Path $SettingsFile -Destination ('{0}{1}' -f $SettingsDestination, $SettingsFile)
