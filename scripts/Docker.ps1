7e603406ecec888c312544f4136ed7d75d419a5a
falkheiland <falkheiland@users.noreply.github.com>
2020-10-14 10:46:15 +0200

    fixed typo

--- scripts/Docker.ps1
+++ scripts/Docker.ps1
@@ -3,5 +3,5 @@ Write-Host "# Docker" -ForegroundColor "Yellow"
 choco install -y Containers -source windowsfeatures
 choco install -y Containers-DisposableClientVM -source windowsfeatures
 choco install -y docker-desktop
-choco install -y docker-docker-toolbox
+choco install -y docker-toolbox
 choco install -y vscode-docker
