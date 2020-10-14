233f586ffe8529b0780ebf481fb70b224fdf09df
falkheiland <falkheiland@users.noreply.github.com>
2020-10-14 15:43:09 +0200

    Update SystemConfiguration.ps1

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -10,6 +10,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
 Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
+Write-Host "## SystemConfiguration - Microsoft Compatibility Appraiser" -ForegroundColor "Yellow"
 <#
 Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\ProgramDataUpdater" | Out-Null
 Disable-ScheduledTask -TaskName "Microsoft\Windows\Autochk\Proxy" | Out-Null
