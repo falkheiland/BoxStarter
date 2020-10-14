62f5c6fcc4029bf744aad96a298710e8fd3b08a2
falkheiland <falkheiland@users.noreply.github.com>
2020-10-14 15:25:06 +0200

    Delete ConfigureWindowsEnvironment.ps1

--- scripts/ConfigureWindowsEnvironment.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "# ConfigureWindowsEnvironment" -ForegroundColor "Yellow"
-
-#Copy-Item -Recurse '~\projects\dotfiles\WindowsPowerShell\*' '~\Documents\WindowsPowerShell\'
-
-# WindowsPowerShell Microsoft.PowerShell_profile.ps1 https://gist.github.com/falkheiland/d221d84a7b2a383854747c127a612a89
-
-# Windows Terminal settings.json https://gist.github.com/falkheiland/a227aeb50dfb380b66a6f4e9c983acff
